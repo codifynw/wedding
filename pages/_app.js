da75e33afe80bd8b010961a1d4303d3ae44769f5
Cody Wilson <cody@codifynw.com>
2022-02-19 16:20:00 -0800

    landing with fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import './index.css'
+import Head from 'next/head'
+import normalize from 'normalize.css'
+import 'modern-normalize/modern-normalize.css'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Head>
+      <title>Jaclyn + Cody</title>
+      <meta name="descritpion" content="Jaclyn + Cody wedding website" />
+      <meta name="keywords" content="Jaclyn + Cody wedding website" />
+      <meta name="author" content="Jaclyn + Cody wedding website" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;1,400&display=swap" rel="stylesheet"></link>
+    </Head>
+    <Component {...pageProps}  />
+  </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,8 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div className="main">
-      <Link href="/birds">
-        <a>Birds Example</a>
-      </Link>
-      <Link href="/boxes">
-        <a>Boxes Example</a>
-      </Link>
+      <h1 className="landing">Jaclyn + Cody</h1>
+      <p>8.14.2022 BEND, OREGON</p>
     </div>
   )
 }

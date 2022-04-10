c05d2852f854154a4581f4372bd68ef8b4c8706e
Cody Wilson <cody.wilson@renovationbrands.com>
2022-04-09 18:34:09 -0700

    embed styles

--- pages/rsvp.js
+++ pages/rsvp.js
@@ -3,7 +3,7 @@ import React, { useRef, useEffect, useState } from "react";
 export default function RSVP() {
   return (
     <div className="RSVP">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfcsv7TNJ0gwj06shAG9d9MWUpmF8oGYz6SD6bGifIvclpBUw/viewform?embedded=true" width="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfcsv7TNJ0gwj06shAG9d9MWUpmF8oGYz6SD6bGifIvclpBUw/viewform?embedded=true" width="100%" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
     </div>
   );
 }

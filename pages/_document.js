229a66e545fc8f172361baa9ad80949312514093
Cody Wilson <cody@codifynw.com>
2022-04-14 16:08:57 -0700

    home page

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
-        <Head>
+      <Head>
         <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0"
-          />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0"
+        />
         <title>Jaclyn + Cody</title>
         <meta name="descritpion" content="Jaclyn + Cody wedding website" />
         <meta name="keywords" content="Jaclyn + Cody wedding website" />
@@ -16,22 +16,37 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;1,400&display=swap" rel="stylesheet"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;1,400&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lustria&display=swap"
+          rel="stylesheet"
+        ></link>
 
         <link
-            rel="preload"
-            href="/fonts/AmalfiCoast.ttf"
-            as="font"
-            crossOrigin=""
-          />
-
-
-        </Head>
+          rel="preload"
+          href="/fonts/AmalfiCoast.ttf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,42 @@
-import React, { useRef, useEffect } from 'react';
-import gsap from 'gsap';
-import Link from 'next/link'
+import React, { useRef, useEffect } from "react";
+import CircleType from "circletype";
+
+import Image from "next/image";
+import birdPNG from "../public/img/birds_icon.png";
+
+import gsap from "gsap";
+import Link from "next/link";
 
 export default function IndexPage() {
+  const circleTop = useRef();
+  const circleBottom = useRef();
+
+  useEffect(() => {
+    new CircleType(circleTop.current).radius(300);
+    new CircleType(circleBottom.current).radius(300).dir(-1);
+  }, []);
+
   return (
     <div className="main">
+      <div className="slider_trans_black"></div>
       <div className="landing-wrap">
-        <h1 className="landing amalfi">Jaclyn + Cody</h1>
-        <div className="landing-date">Bend, Oregon - 8.14.2022</div>
-        <div className="landing-bottom">Details Coming Soon!</div>
+        <div className="landing-banner">
+          <div ref={circleTop} className="landing-top altColor">
+            Save The Date
+          </div>
+          <div className="love_birds">
+            <Image src={birdPNG} alt="" title="" />
+          </div>
+          <h1 className="landing amalfi">
+            Jaclyn <span className="altColor amalfi">& </span> Cody
+          </h1>
+          <div className="landing-date">8.14.2022</div>
+          <div ref={circleBottom} className="landing-bottom altColor">
+            Bend, Oregon
+          </div>
+        </div>
       </div>
-      <div className="nav-wrap hide">
+      <div className="nav-wrap">
         {/* <Link href="/rsvp">
           <a className="nav-link">RSVP</a>  
         </Link> */}
@@ -20,24 +46,33 @@ export default function IndexPage() {
         <Link href="/registry">
           <a className="nav-link">REGISTRY</a>
         </Link> */}
-        <Link href="/details">
-          <a className="nav-link">Details</a>
+        <Link href="/">
+          <div className="nav-link">
+            <a className="bold"> HOME</a>
+          </div>
+        </Link>
+        {/* <Link href="/details">
+          <div className="nav-link">
+            <a>Details</a>
+          </div>
         </Link>
         <Link href="/accomodations">
-          <a className="nav-link">Accomodations</a>
-        </Link>
+          <div className="nav-link">
+            <a>Accomodations</a>
+          </div>
+        </Link> */}
         <Link href="/rsvp">
-          <a className="nav-link">RSVP</a>
+          <div className="nav-link">
+            <a>RSVP</a>
+          </div>
         </Link>
       </div>
       <section className="travel bg-mauve hide">
         <div className="travel-wrap">
           <h2 className="travel-heading amalfi">Travel + </h2>
-          <p>
-
-          </p>
+          <p></p>
         </div>
       </section>
     </div>
-  )
+  );
 }
